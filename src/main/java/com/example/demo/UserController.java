2f6bbfe6bedad0217c4e80c09d0fa27c8b7e6025
chutimon <chutimon.chaiyawong@allianz.com>
2020-08-05 14:41:18 +0700

    step_03

--- src/main/java/com/example/demo/UserController.java
+++ src/main/java/com/example/demo/UserController.java
@@ -1,29 +1,37 @@
+
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class UserController {
 
-//    @GetMapping("/users")
-//    public List<UsersResponse> getAllUser(){
-//        List<UsersResponse> users = new ArrayList<>();
-//        users.add(new UsersResponse(1, "User 1"));
-//        users.add(new UsersResponse(2, "User 2"));
-//        users.add(new UsersResponse(3, "User 3"));
-//        return users;
-//    }
-//
-//    @GetMapping("/users/{id}")
-//    public UsersResponse getUserById( @PathVariable int id){
-//        return new UsersResponse(id, "User "+ id);
-//    }
     @GetMapping("/users")
-    public String getPage(@RequestParam(defaultValue = "1") String page, @RequestParam(name = "item_per_page" ,defaultValue = "10") String itemPerPage) {
-        return "Page: " + page + " , itemPerPage: " + itemPerPage;
+    public PagingResponse getAllUser(
+            @RequestParam(defaultValue = "1") int page,
+            @RequestParam(name = "item_per_page" ,defaultValue = "10") int itemPerPage) {
+
+        PagingResponse pagingResponse = new PagingResponse(page, itemPerPage);
+        List<UsersResponse> usersResponseList = new ArrayList<>();
+        usersResponseList.add(new UsersResponse(1, "User 1"));
+        usersResponseList.add(new UsersResponse(2, "User 2"));
+        usersResponseList.add(new UsersResponse(3, "User 3"));
+        pagingResponse.setUsersResponse(usersResponseList);
+        return pagingResponse;
+    }
+
+    @GetMapping("/users/{id}")
+    public UsersResponse getUserById(@PathVariable int id) {
+        return new UsersResponse(id, "User " + id);
+    }
+
+    @PostMapping("/users")
+    public UsersResponse createNewUser(@RequestBody NewUserRequest request){
+        return new UsersResponse(0, request.getName()+request.getAge());
     }
 
 }
